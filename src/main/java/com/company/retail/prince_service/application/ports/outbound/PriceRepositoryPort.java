da26a9a88d9619a3cbe0ebef3b0918ddab35551b
jaimart <106272862+jaimart@users.noreply.github.com>
2026-06-19 22:57:24 -0500

    nombre de proyecto

--- src/main/java/com/company/retail/prince_service/application/ports/outbound/PriceRepositoryPort.java
+++
@@ -1,9 +0,0 @@
-package com.company.retail.prince_service.application.ports.outbound;
-
-import java.time.LocalDateTime;
-import java.util.Optional;
-import com.company.retail.prince_service.domain.model.Price;
-
-public interface PriceRepositoryPort {
-    Optional<Price> findApplicablePrice(LocalDateTime applicationDate, Integer productId, Integer brandId);
-}
